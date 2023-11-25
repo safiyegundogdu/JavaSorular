ca91c12ab5d72587db3813260e37245c632ef4ae
Safiye <safiye.dayan@gmail.com>
2023-11-25 21:43:32 +0300

    fourth

--- src/main/java/algoritmasorulari/SayiToplama.java
+++ src/main/java/algoritmasorulari/SayiToplama.java
@@ -4,19 +4,36 @@ import java.util.Scanner;
 
 public class SayiToplama {
     public static void main(String[] args) {
+
+        //while
         //0 girilene kadar, girilmiş tüm sayıların
         //toplanması...
         int toplam=0;
-        int sayi;
+        int sayi ;
         while(true)
         {
             Scanner scan = new Scanner(System.in);
             System.out.print("Bir sayi girin: ");
             sayi = scan.nextInt();
             if(sayi==0) break;
-            toplam=toplam + sayi;
+            toplam=toplam+  sayi;
         }
         System.out.println("Sayıların Toplamı: "+toplam);
+
+        //do...while()
+
+        int sum=0;
+        int sayi1 ;
+        do
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Bir sayi girin: ");
+            sayi1 = scan.nextInt();
+            if(sayi1==0) break;
+            sum=sum+  sayi;
+        }while(true);
+        System.out.println("Sayıların Toplamı: "+toplam);
+
     }
     }
 
